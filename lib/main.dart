29ea25e8370ed52880647435fbe922aec9da1b83
thangapandi8111 <thangapandibalasubaramaniyan@gmail.com>
2025-06-16 15:25:06 +0530

    imports addes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,3 +22,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// https://reqres.in/api/users?page=1
